f8406231dddee895e5832746d42c05488492215a
Artemy Ivanov <ghostsmans@gmail.com>
2022-10-07 09:49:30 +0300

    Исправлена ошибка в расчёте вязкости

--- fluid_main.py
+++ fluid_main.py
@@ -30,24 +30,41 @@ def sigma_calc(vc, na):
     return (0.302 * vc / na) ** (1.0 / 3.0)
 
 
+def sigmaij_calc(sigma_ii, sigma_jj):
+    return ((sigma_ii ** 3 + sigma_jj ** 3) / 2) ** (1 / 3)
+
+
+def sigma_setup(vc, na):
+    sigmamain = empty(len(vc))
+    for i in range(len(vc)):
+        sigmamain[i] = sigma_calc(vc[i], na)
+    returnable = empty([len(vc), len(vc)])
+    for i in range(len(vc)):
+        returnable[i][i] = sigmamain[i]
+        for j in range(i + 1, len(vc)):
+            returnable[i][j] = returnable[j][i] = sigmaij_calc(sigmamain[i], sigmamain[j])
+    return returnable
+
+
+def eps_setup(sigma, kb, tc):
+    eps = empty([len(sigma), len(sigma)])
+    for i in range(len(sigma)):
+        eps[i][i] = eps_calc(kb, tc[i])
+    for i in range(len(sigma)):
+        for j in range(i + 1, len(sigma)):
+            eps[i][j] = eps[j][i] = (eps[i][i] * sigma[i][i] ** 3 + eps[j][j] * sigma[j][j] ** 3) / 2.0 / sigma[i][
+                j] ** 3
+    return eps
+
+
 # вынести tc и vc в внешний вектор
 def viscosity(x, mw_i, nu_i, t):
     kb = 1.380649 * 1e-23
     na = 6.022 * 1e23
-    tc_ii = 516.25
-    tc_jj = 540.61
-    vc_ii = 182.49 * 1e-6
-    vc_jj = 428.64 * 1e-6
-    eps_ii = eps_calc(kb, tc_ii)
-    eps_jj = eps_calc(kb, tc_jj)
-    sigma_ii = sigma_calc(vc_ii, na)
-    sigma_jj = sigma_calc(vc_jj, na)
-    sigma_ij = ((sigma_ii ** 3 + sigma_jj ** 3) / 2) ** (1.0 / 3.0)
-    eps_ij = (eps_ii * sigma_ii ** 3 + eps_jj * sigma_jj ** 3) / 2.0 / sigma_ij ** 3
-    sigma = [[sigma_ii, sigma_ij],
-             [sigma_ij, sigma_jj]]
-    eps = [[eps_ii, eps_ij],
-           [eps_ij, eps_jj]]
+    tc = [516.25, 540.61]
+    vc = [182.49 * 1e-6, 428.64 * 1e-6]
+    sigma = sigma_setup(vc, na)
+    eps = eps_setup(sigma, kb, tc)
     sigma_x = 0.0
     eps_x = 0.0
     nu = 0.0
@@ -57,7 +74,7 @@ def viscosity(x, mw_i, nu_i, t):
         mwx += mw_i[i] * x[i]
         for j in range(0, 2):
             sigma_x += x[i] * x[j] * sigma[j][i] ** 3
-            eps_x += x[i] * x[j] * eps[j][i] * sigma[i][j] ** 3
+            eps_x += x[i] * x[j] * eps[j][i] * sigma[j][i] ** 3
     sigma_x = sigma_x ** (1.0 / 3.0)
     eps_x /= sigma_x ** 3
     t_ast = kb * t / eps_x
@@ -66,12 +83,13 @@ def viscosity(x, mw_i, nu_i, t):
     coeff = [1.06036, 0.15610, 0.19300, 0.47635, 1.03587, 1.52996, 1.76474, 3.89411]
     # можно подогнать
     b = [0.062692, 4.095577, -8.743269 * 1e-6, 11.124920, 2.542477 * 1e-6, 14.863984]
+
     collision_integral = coeff[0] / t_ast ** coeff[1] + coeff[2] / exp(coeff[3] * t_ast) + coeff[4] / exp(
         coeff[5] * t_ast) + coeff[6] / exp(coeff[7] * t_ast)
     ac = 0.95
     visc_0_ast = 0.17629 * t_ast ** (1.0 / 2.0) * ac / collision_integral
-    delta_visc_ast = b[0] * (exp(b[1] * rho_ast) - 1) + b[2] * (exp(b[3]) * rho_ast) + b[4] * t_ast ** (
-            -2 ** exp(b[5] * rho_ast) - 1)
+    delta_visc_ast = b[0] * (exp(b[1] * rho_ast) - 1) + b[2] * (exp(b[3] * rho_ast) - 1) + b[4] * t_ast ** (-2) * (
+                exp(b[5] * rho_ast) - 1)
     visc_ast = visc_0_ast + delta_visc_ast
     visc = visc_ast * (mwx * eps_x / na) ** 0.5 / (sigma_x ** 2)
     return visc
