8ee7df3982212ae3d871a4faf0a668ad4cf3121f
Артемий Андреевич Иванов <ghostsmans@gmail.com>
2022-10-07 06:40:16 +0300

    Комментарии с изменениями

--- fluid_main.py
+++ fluid_main.py
@@ -22,6 +22,15 @@ class BetterArr(array):
         return negative(self)
 
 
+def eps_calc(kb, tc):
+    return kb * tc / 1.2593
+
+
+def sigma_calc(vc, na):
+    return (0.302 * vc / na) ** (1.0 / 3.0)
+
+
+# вынести tc и vc в внешний вектор
 def viscosity(x, mw_i, nu_i, t):
     kb = 1.380649 * 1e-23
     na = 6.022 * 1e23
@@ -29,12 +38,12 @@ def viscosity(x, mw_i, nu_i, t):
     tc_jj = 540.61
     vc_ii = 182.49 * 1e-6
     vc_jj = 428.64 * 1e-6
-    eps_ii = kb * tc_ii / 1.2593
-    eps_jj = kb * tc_jj / 1.2593
-    sigma_ii = (0.302 * vc_ii / na) ** (1 / 3)
-    sigma_jj = (0.302 * vc_jj / na) ** (1 / 3)
-    sigma_ij = ((sigma_ii ** 3 + sigma_jj ** 3) / 2) ** (1 / 3)
-    eps_ij = (eps_ii * sigma_ii ** 3 + eps_jj * sigma_jj ** 3) / 2 / sigma_ij ** 3
+    eps_ii = eps_calc(kb, tc_ii)
+    eps_jj = eps_calc(kb, tc_jj)
+    sigma_ii = sigma_calc(vc_ii, na)
+    sigma_jj = sigma_calc(vc_jj, na)
+    sigma_ij = ((sigma_ii ** 3 + sigma_jj ** 3) / 2) ** (1.0 / 3.0)
+    eps_ij = (eps_ii * sigma_ii ** 3 + eps_jj * sigma_jj ** 3) / 2.0 / sigma_ij ** 3
     sigma = [[sigma_ii, sigma_ij],
              [sigma_ij, sigma_jj]]
     eps = [[eps_ii, eps_ij],
@@ -49,16 +58,18 @@ def viscosity(x, mw_i, nu_i, t):
         for j in range(0, 2):
             sigma_x += x[i] * x[j] * sigma[j][i] ** 3
             eps_x += x[i] * x[j] * eps[j][i] * sigma[i][j] ** 3
-    sigma_x = sigma_x ** (1 / 3)
+    sigma_x = sigma_x ** (1.0 / 3.0)
     eps_x /= sigma_x ** 3
     t_ast = kb * t / eps_x
     rho_ast = na * (sigma_x ** 3) / nu
+    # не меняется
     coeff = [1.06036, 0.15610, 0.19300, 0.47635, 1.03587, 1.52996, 1.76474, 3.89411]
+    # можно подогнать
     b = [0.062692, 4.095577, -8.743269 * 1e-6, 11.124920, 2.542477 * 1e-6, 14.863984]
     collision_integral = coeff[0] / t_ast ** coeff[1] + coeff[2] / exp(coeff[3] * t_ast) + coeff[4] / exp(
         coeff[5] * t_ast) + coeff[6] / exp(coeff[7] * t_ast)
     ac = 0.95
-    visc_0_ast = 0.17629 * t_ast ** (1 / 2) * ac / collision_integral
+    visc_0_ast = 0.17629 * t_ast ** (1.0 / 2.0) * ac / collision_integral
     delta_visc_ast = b[0] * (exp(b[1] * rho_ast) - 1) + b[2] * (exp(b[3]) * rho_ast) + b[4] * t_ast ** (
             -2 ** exp(b[5] * rho_ast) - 1)
     visc_ast = visc_0_ast + delta_visc_ast
@@ -78,11 +89,14 @@ def LJ_fluid():
     P = BetterArr([0.1, 20, 40, 60, 80, 100])
     Mw = BetterArr([46.07, 100.2]) / 1000
     constC = 0.0894
+
+    # настроечные параметры
     B0 = BetterArr([1e2, 1e2])
     B1 = BetterArr([0.0009, 0.0009])
     alpha_n = BetterArr([0.002, 0.004])
     nu0 = BetterArr([1.04958e-4, 4.76409e-4])
     nuP0 = nu0 * exp(-alpha_n * T)
+
     B = B0 * exp(-B1 * T)
     vvv = empty((len(P), len(vexp)))
     for j in range(0, len(P)):
