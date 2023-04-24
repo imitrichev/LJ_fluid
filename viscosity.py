a05932db616300be330fcdeb40a5731692383a7f
funnemonkee <ghostsmans@gmail.com>
2023-04-24 16:19:02 +0300

    Переписал трёхпараметрическую экспоненту под новую функцию aad_new

--- viscosity.py
+++ viscosity.py
@@ -1,6 +1,6 @@
 from math import exp
 from dataclasses import dataclass
-from common import aad, load_from_directory
+from common import aad, ViscData, aad_new
 from scipy.optimize import minimize, Bounds
 from os import listdir
 import matplotlib.pyplot as plt
@@ -8,18 +8,22 @@ from numpy import arange
 
 
 class VFT:
-    def __init__(self, temps: list[float], visc_pref: list[float], x0: list[float], pref=0):
-        self.temps = temps
-        self.visc_pref = visc_pref
-        self.params = x0.copy()
-        self.x0 = x0
+    def __init__(self, mu_data: dict[float:dict[float:float]], pref: float):
+        self.mu_data = mu_data
         self.pref = pref
-        self.setup_def()
+        self.params = [0, 0, 0, 0, 0, 0, 0]
 
     def setup_def(self):
-        args = (self.temps, self.visc_pref)
-        test = minimize(fun=aadDEF, x0=self.x0[:3], method='Nelder-Mead', args=args, tol=1e-5)
-        print(test.message)
+        visc_pref = list(self.mu_data[pref].values())
+        temps = list(self.mu_data[pref].keys())
+
+        temp_x = [1e-3, 1000, 665.2620524970494, 0, 0, 0, 0]
+        if visc_pref[0] < visc_pref[-1]:
+            temp_x = [1e-3, 8000, 665.2620524970494, 0, 0, 0, 0]
+
+        x0 = temp_x
+        args = (DEF, temps, visc_pref)
+        test = minimize(fun=aad_new, x0=x0[:3], method='Nelder-Mead', args=args, tol=1e-7)
         self.params[0], self.params[1], self.params[2] = test.x
 
     def setup_hg(self):
@@ -34,15 +38,14 @@ class VFT:
                   self.params[2], t, p, self.pref)
 
 
+class MuMix:
+    def __init__(self, ):
+        pass
+
+
 def DEF(params, point):
     d, e, f = params
     return d * exp(e / (point + 273.15 - f))
-
-
-def aadDEF(x, temp, visc):
-    visccalc = [DEF(x, temp[i]) for i in range(len(temp))]
-    aad_calc = aad(visc, visccalc)
-    return aad_calc
 
 
 # Замена наименований параметров в связи с требованиями scipy
@@ -60,20 +63,22 @@ def aadHG(params, static_params, points, visc):
 
 if __name__ == "__main__":
     substance = "R115"
-    visc = load_from_directory(substance, -1)
-
+    data = ViscData(substance)
     pref = 0.06
+
+    visc = data.visc_k
     temps = list(visc[pref].keys())
     viscs = [visc[pref][i] for i in temps]
-    x0 = [0, -3839.870717407848, 665.2620524970494, 0, 0, 0, 0]
 
-    test = VFT(temps, viscs, x0)
+    test = VFT(visc, pref)
+    test.setup_def()
     print(f'D = {test.params[0]}\nE = {test.params[1]}\nF = {test.params[2]}\n')
     print(f'AAD = {aadDEF(test.params[:3], temps, viscs)}')
 
     fig, ax = plt.subplots()
     ax.scatter(temps, viscs, label="Экспериментальные данные")
-    ax.plot(arange(temps[0], temps[-1]+1), [test.calc_visc_def(i) for i in arange(temps[0], temps[-1]+1)], "r", label="Расчётные значения")
+    ax.plot(arange(temps[0], temps[-1] + 1), [test.calc_visc_def(i) for i in arange(temps[0], temps[-1] + 1)], "r",
+            label="Расчётные значения")
     ax.set_ylabel("Mu, cP")
     ax.set_xlabel("T, Гр. Цельсия")
     ax.set_title("mu = f(t)")
