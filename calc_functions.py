3b6601d415add360ae908fe2b064a69cab78697b
SpaghetNMeatballs <ghostsmans@gmail.com>
2023-04-30 15:43:16 +0300

    Избавился от aad в пользу aad_new и добавил отдельный файл для расчётных функций (#6)
    * Добавил график плотности от давления
    
    * Изменения aad_new
    
    * Переписал трёхпараметрическую экспоненту под новую функцию aad_new
    
    * Переписал трёхпараметрическую экспоненту под новую функцию aad_new
    
    * Улучшения загрузчика из директорий и реализация VFT
    
    * Вынес расчётные функции в отдельный файл и добавил функцию Грунберга-Ниссана
    
    * Переписал функции расчёта плотности под новый стандарт aad_new
    
    * Добавлены функции для расчёта плотности
    
    * Переписал плотность под стандарт aad_new
    
    * Исправил ошибку в порядке переменных в setup_rho_full

---
+++ calc_functions.py
@@ -0,0 +1,50 @@
+from math import exp, log
+
+
+# Params = [D, E, F], point = t
+def DEF(params: list[float], point: float) -> float:
+    d, e, f = params
+    return d * exp(e / (point + 273.15 - f))
+
+
+# Params = [G0, G1, G2, H], Static_Params = все для DEF и pref, point = [p, t]
+def HG(params: list[float], static_params: list[float], point: list[float]) -> float:
+    g0, g1, g2, h = params
+    d, e, f, pref = static_params
+    p, t = point
+    result = DEF([d, e, f], t)
+    top = (p + g0 + g1 * t + g2 * t ** 2)
+    bot = (pref + g0 + g1 * t + g2 * t ** 2)
+    second_part = (top / bot) ** h
+    return result * second_part
+
+
+# Params = d, Static_Params = x, point = mu
+def grunberg_nissan(params: list[list[float]], static_params: list[float], point: list[float]) -> float:
+    d = params
+    x = static_params
+    mu = point
+    visc_sum = sum([x[i] * log(mu[i]) for i in range(2)])
+    param_sum = sum([sum([x[i] * x[j] * d[i][j]] for j in range(2)) for i in range(2)])
+    return visc_sum + param_sum
+
+
+def rho_t(params: float, static_params: list[float], point: float) -> float:
+    e = params
+    rho0, t0 = static_params
+    t = point
+    return rho0 / (1 + e * (t - t0))
+
+
+def rho_p(params: float, static_params: list[float], point: float) -> float:
+    beta = params
+    rho0, p0 = static_params
+    p = point
+    return rho0 / (1 - (p - p0) / beta)
+
+
+def rho_full(params: list[float], static_params: list[float], point: list[float]) -> float:
+    e, beta = params
+    rho0, p0, t0 = static_params
+    p, t = point
+    return (rho0 / (1 + beta * (t - t0))) / (1 - (p - p0) / e)
--- density.py
+++ density.py
@@ -1,109 +1,85 @@
-from common import aad
+from common import aad, load_from_directory, aad_new, ViscData
+from calc_functions import rho_t, rho_p, rho_full
 from scipy.optimize import minimize
 import matplotlib.pyplot as plt
-from os import listdir
 from numpy import arange
 
 
 class Density:
-    def __init__(self, rho0, t0, p0):
+    def __init__(self, rho0: float, point0: list[float]):
         self.rho0 = rho0
-        self.t0 = t0
-        self.p0 = p0
+        self.p0, self.t0 = point0
         self.beta = 0
-        self.e = 10
+        self.e = 3
 
-    def calc_rho1(self, t, beta=None):
-        if not beta:
-            beta = self.beta
-        return self.rho0 / (1 + beta * (t - self.t0))
+    def calc_rho_t(self, t: float) -> float:
+        return rho_t(self.beta, [self.rho0, self.t0], t)
 
-    def calc_rho2(self, p, e=None):
-        if not e:
-            e = self.e
-        return self.rho0 / (1 - (p - self.p0) / e)
+    def calc_rho_p(self, p: float) -> float:
+        return rho_p(self.beta, [self.rho0, self.p0], p)
 
-    def calc_rho_full(self, p, t, e=None, beta=None):
-        if not e:
-            e = self.e
-        if not beta:
-            beta = self.beta
-        return (self.rho0 / (1 + beta * (t - self.t0))) / (1 - (p - self.p0) / e)
+    def calc_rho_full(self, point: list[float]) -> float:
+        p, t = point
+        return rho_full([self.e, self.beta], [self.rho0, self.p0, self.t0], [p, t])
 
-    def aad_rho1(self, beta, temp, rho):
-        rho_calc = [self.calc_rho1(i, beta) for i in temp]
-        return aad(rho, rho_calc)
+    def setup_rho_t(self, temps: list[float], rho: list[float]) -> bool:
+        args = (rho_t, temps, rho, [self.rho0, self.t0])
+        result = minimize(fun=aad_new, x0=0, args=args)
+        self.beta = result.x[0]
+        return result.success
 
-    def aad_rho2(self, e, p, rho):
-        rho_calc = [self.calc_rho2(i, e) for i in p]
-        return aad(rho, rho_calc)
+    def setup_rho_p(self, p: list[float], rho: list[float]) -> bool:
+        args = (rho_p, p, rho, [self.rho0, self.p0])
+        result = minimize(fun=aad_new, x0=3, args=args)
+        self.e = result.x[0]
+        return result.success
 
-    def aad_rho_full(self, params, rho):
-        e = params[0]
-        beta = params[1]
-        rho_calc = []
-        rho_exp = []
-        for p_cur in list(rho.keys()):
-            for t_cur in list(rho[p_cur].keys()):
-                rho_calc.append(self.calc_rho_full(p_cur, t_cur, e, beta))
-                rho_exp.append(rho[p_cur][t_cur])
-        test = aad(rho_calc, rho_exp)
-        return test
-
-    def setup_rho1(self, temps, rho):
-        args = (temps, rho)
-        test = minimize(fun=self.aad_rho1, x0=0, args=args)
-        self.beta = test.x[0]
-
-    def setup_rho2(self, p, rho):
-        args = (p, rho)
-        test = minimize(fun=self.aad_rho2, x0=3, args=args)
-        self.e = test.x[0]
-
-    def setup_rho_full(self, rho):
-        args = (rho)
-        test = minimize(fun=self.aad_rho_full, method='Nelder-Mead', x0=[3, 0], args=args)
-        self.e = test.x[0]
-        self.beta = test.x[1]
+    def setup_rho_full(self, rho, points) -> bool:
+        static_params = (self.rho0, self.p0, self.t0)
+        args = (rho_full, points, rho, static_params)
+        print(aad_new([3, 0], rho_full, points, rho, static_params))
+        result = minimize(fun=aad_new, method='Nelder-Mead', x0=[3, 0], args=args,
+                          options={'maxiter': 100000, 'maxfev': 100000})
+        self.e, self.beta = result.x
+        print(aad_new([self.e, self.beta], rho_full, points, rho, static_params))
+        print(result.message)
+        return result.success
 
 
 if __name__ == "__main__":
-    rho = {}
     substance = "R22"
-    for file in listdir(substance):
-        with open(substance + "\\" + file, "r") as f:
-            text = f.read()
-            p = float(file[:-4])
-            rho[p] = {}
-            for line in text.split('\n'):
-                cur_line = line.split(' ')
-                rho[p][float(cur_line[0])] = float(cur_line[2])
-    p = list(rho.keys())
-    graph_numb = [0, 0]
-    p0 = 0.05
-    temps = list(rho[p0].keys())
-    t0 = -80
-    rho_p_dynamic = [rho[i][t0] for i in p]
-    rho_t = list(rho[p0].values())
-    rho_p = [rho[i][t0] for i in p]
-    rho0 = rho[p0][t0]
-    test = Density(rho0, t0, p0)
-    test.setup_rho_full(rho)
-    fig, ax = plt.subplots(2, 4)
+    substance_data = ViscData(substance)
+    points_to_process = substance_data.get_all_points(0)
+    rho = [i[2] for i in points_to_process]
+    points = [i[:2] for i in points_to_process]
+    rh0, p0, t0 = substance_data.get_middle_point(0)
+    density_calc = Density(rh0, [p0, t0])
+    if not density_calc.setup_rho_full(rho, points):
+        print('Error while optimizing')
 
-    t_exp = list(rho[p0].keys())
-    t_calc = arange(t_exp[0], t_exp[-1], 0.5)
-    for i in range(len(p)):
-        p_cur = p[i]
+    fig, ax = plt.subplots(2, 4)
+    p_arr = list(substance_data.density.keys())
+    ax[0][0].scatter(x=p_arr, y=[substance_data.density[i][t0] for i in p_arr])
+    calc_p = arange(0.04, 0.101, 0.001)
+    calc_rho = [density_calc.calc_rho_full([i, t0]) for i in calc_p]
+    ax[0][0].plot(calc_p, calc_rho, "r", label="Расчётные значения")
+    for i in range(len(p_arr)):
+        p_cur = p_arr[i]
         graphy = (i + 1) // 4
         graphx = (i + 1) % 4
-        exp_temps = list(rho[p_cur].keys())
-        t_arr = arange(exp_temps[0], exp_temps[-1], 0.5)
-        ax[graphy][graphx].plot(t_arr, [test.calc_rho_full(p_cur, j) for j in t_arr], "r", label="Calculated")
-        ax[graphy][graphx].plot(exp_temps, list(rho[p_cur].values()), "b", label="Expected")
-        ax[graphy][graphx].set_title("P = %f" % p_cur)
-    print("Beta = %f" % test.beta)
-    print("E = %f" % test.e)
-    print(test.aad_rho2(test.e, p, rho_p))
-    plt.legend()
+        exp_values = substance_data.get_by_pressure(0, p_arr[i])
+        exp_temps = [j[1] for j in exp_values]
+        exp_rho = [j[2] for j in exp_values]
+        step = 0.01
+        t_calc = arange(exp_temps[0], exp_temps[-1] + step, step)
+        rho_calc = [density_calc.calc_rho_full([p_cur, j]) for j in t_calc]
+        ax[graphy][graphx].scatter(x=exp_temps, y=exp_rho, label="Экспериментальные данные")
+        ax[graphy][graphx].plot(t_calc, rho_calc, "r", label="Расчётные значения")
+        ax[graphy][graphx].set_title("p = %.2f MPa" % p_cur)
+        ax[graphy][graphx].set_ylabel("Rho, м3/кг")
+        ax[graphy][graphx].set_xlabel("P, МПа")
+        ax[graphy][graphx].legend()
+
+    print("Beta = %f" % density_calc.beta)
+    print("E = %f" % density_calc.e)
     plt.show()
